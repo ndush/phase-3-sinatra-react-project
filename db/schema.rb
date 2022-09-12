2c96f96db7b9170a632b4b6f8f7a485b87595885
damaris <mdamarisnduku@gmail.com>
2022-09-12 09:40:05 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,32 +1,32 @@
-# # This file is auto-generated from the current state of the database. Instead
-# # of editing this file, please use the migrations feature of Active Record to
-# # incrementally modify your database, and then regenerate this schema definition.
-# #
-# # This file is the source Rails uses to define your schema when running `bin/rails
-# # db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# # be faster and is potentially less error prone than running all of your
-# # migrations from scratch. Old migrations may fail to apply correctly if those
-# # migrations use external dependencies or application code.
-# #
-# # It's strongly recommended that you check this file into your version control system.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-# ActiveRecord::Schema.define(version: 2022_09_10_171726) do
+ActiveRecord::Schema.define(version: 2022_09_10_171726) do
 
-#   create_table "orders", force: :cascade do |t|
-#     t.string "quantity"
-#     t.integer "supplier_id"
-#     t.integer "product_id"
-#   end
+  create_table "orders", force: :cascade do |t|
+    t.string "quantity"
+    t.integer "supplier_id"
+    t.integer "product_id"
+  end
 
-#   create_table "products", force: :cascade do |t|
-#     t.string "product_name"
-#     t.integer "unit_price"
-#     t.string "description"
-#     t.string "image_url"
-#   end
+  create_table "products", force: :cascade do |t|
+    t.string "product_name"
+    t.integer "unit_price"
+    t.string "description"
+    t.string "image_url"
+  end
 
-#   create_table "suppliers", force: :cascade do |t|
-#     t.string "supplier_name"
-#   end
+  create_table "suppliers", force: :cascade do |t|
+    t.string "supplier_name"
+  end
 
-# end
+end
